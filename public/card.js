1e8c59190a88ddf561cbd069bef8c357626fc1dc
= <childebr@ualberta.ca>
2021-11-21 21:56:12 -0700

    renamed files

--- public/card.js
+++ public/card.js
@@ -504,7 +504,7 @@ var natUriCard = new Card({
   <br><br>
   <b>When:</b> Thursday, November 18, 17:00-18:00 MST<br>
   <b>Where:</b> Virtual - Link will be sent to those who RSVP.`,
-  image: "/images/event/misc./n+uri.png",
+  image: "/images/event/misc./nuri.png",
   buttonText: "RSVP",
   link: "/rsvp.html",
   location: "Virtual - Link will be sent to those who RSVP.",
